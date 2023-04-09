17b37967e464d5421dd51e722a3561f92f313462
Dmitry <k1791@mail.ru>
2023-04-10 00:40:06 +0500

    step7/ users_functions

--- data/API_connect.py
+++ data/API_connect.py
@@ -4,12 +4,14 @@ import os
 from abc import ABC, abstractmethod
 import time
 from requests import get, post, put, delete
-
+from operator import *
 # import isodate
+
+
 
 class Engine(ABC):
     @abstractmethod
-    def get_vacancies(self, vacancy):
+    def get_vacancies(self):
         pass
 
     # @staticmethod
@@ -17,15 +19,85 @@ class Engine(ABC):
     #     """ Возвращает экземпляр класса Connector """
     #     pass
 
-class SuperJobAPI (Engine):
-    """"Класс HH"""
+class Vacancy ():
+    """"Класс Vacancy"""
+
     list = []
 
-    def __init__(self, vacancy):
+    def __init__(self, platforms,search_query,top_n):
+
+        self.platforms = platforms  # платформа 1 - "HeadHunter", 2- "SuperJob", 3 - "HeadHunter"+"SuperJob"
+        self.search_query =search_query #  поисковый запрос
+        self.top_n = top_n # количество вакансий для вывода
+        self.vacancy = self.get_vacancies()
+
+
+        # self.platforms = platform # платформа ["HeadHunter", "SuperJob"]
+        # self.profession = '' # название вакансии
+        # self.candidat = '' # 	Требования к кандидату
+        # self.work = '' # Должностные обязанности
+        # self.compensation = '' # Условия работы
+        # self.profession_url = '' # ссылка на вакансию
+        # self.payment_from = None #Сумма оклада от
+        # self.payment_to =None #Сумма оклада до
+        # self.currency = '' # Валюта. Список возможных значений:  rub — рубль  uah — гривна  uzs — сум
+        pass
+
+    def get_vacancies(self):
+
+        if self.platforms in [1, 3]: # загрузка платформы 1 - "HeadHunter"
+            hh_vacancy = HeadHunterAPI(self.search_query,self.top_n)
+            file1 = hh_vacancy.file
+            f= open(file1, mode='r', encoding='utf-8')
+            data = json.load(f)
+            f.close()
+            for i in data:
+                self.list.append({"platforms":"HeadHunter", # платформа ["HeadHunter", "SuperJob"]
+                             "profession":i.get('items',{})[0].get("name"), # название вакансии
+                             "candidat":i.get('items',{})[0].get("snippet",{}).get("requirement"), # 	Требования к кандидату
+                             "work": i.get('items',{})[0].get("snippet",{}).get("responsibility"), # Должностные обязанности
+                             "compensation": i.get('items',{})[0].get("working_days"), # Условия работы
+                             "profession_url":i.get('items',{})[0].get("url"), # ссылка на вакансию
+                             "payment_from":i.get('items',{})[0].get("salary",{}).get("from"), #Сумма оклада от
+                             "payment_to":i.get('items',{})[0].get("salary",{}).get("to"), #Сумма оклада до
+                             "currency":i.get('items',{})[0].get("salary",{}).get("currency") # Валюта. Список возможных значений:  rub — рубль  uah — гривна  uzs — сум
+                             })
+
+
+
+        if self.platforms in [2, 3]: # загрузка платформы 2- "SuperJob"
+            sj_vacancy = SuperJobAPI(self.search_query,self.top_n)
+            file2 = sj_vacancy.file
+            f = open(file2, mode='r', encoding='utf-8')
+            data = json.load(f)
+            f.close()
+            for i in data:
+                self.list.append({"platforms": "SuperJob", # платформа ["HeadHunter", "SuperJob"]
+                             "profession": i.get('objects',{})[0].get("profession"), # название вакансии
+                             "candidat": i.get('objects',{})[0].get("candidat"), # 	Требования к кандидату
+                             "work": i.get('objects',{})[0].get("work"), # Должностные обязанности
+                             "compensation": i.get('objects',{})[0].get("compensation"), # Условия работы
+                             "profession_url": i.get('objects',{})[0].get("profession_url"), # ссылка на вакансию
+                             "payment_from": i.get('objects',{})[0].get("payment_from"), #Сумма оклада от
+                             "payment_to": i.get('objects',{})[0].get("payment_to"), #Сумма оклада до
+                             "currency": i.get('objects',{})[0].get("currency") # Валюта. Список возможных значений:  rub — рубль  uah — гривна  uzs — сум
+                             })
+
+        sorted(self.list, reverse=True, key= itemgetter ("payment_to"))
+
+        return self.list
+
+
+
+class SuperJobAPI (Engine):
+    """"Класс SuperJobAPI"""
+    list = []
+
+    def __init__(self, vacancy, top_n):
         self.vacancy = vacancy
         self.file = '../data/get_SJ.json'
         self.url = 'https://api.superjob.ru/2.0/vacancies'
-        self.pages = 10
+        self.pages = top_n
         self.job_list = self.get_vacancies()
 
         pass
@@ -63,11 +135,11 @@ class SuperJobAPI (Engine):
             # Полученное значение используем для формирования имени документа
 
             # Проверка на последнюю страницу, если вакансий меньше 2000
-            if (r_page['pages'] - page) <= 1:
-                break
+            # if (r_page['pages'] - page) <= 1:
+            #     break
 
-                # Необязательная задержка, но чтобы не нагружать сервисы hh, оставим. 5 сек мы может подождать
-                time.sleep(0.5)
+            # Необязательная задержка, но чтобы не нагружать сервисы hh, оставим. 5 сек мы может подождать
+            time.sleep(0.5)
 
         # Создаем новый документ, записываем в него ответ запроса, после закрываем
         f = open(self.file, mode='w', encoding='utf8')
@@ -79,14 +151,14 @@ class SuperJobAPI (Engine):
 
 
 class HeadHunterAPI (Engine):
-    """"Класс HH"""
+    """"Класс HeadHunterAPI"""
     list = []
 
-    def __init__(self, vacancy):
+    def __init__(self, vacancy, top_n):
         self.vacancy = vacancy
         self.file = '../data/get_HH.json'
         self.url ='https://api.hh.ru/vacancies'
-        self.pages = 10
+        self.pages = top_n
         self.job_list = self.get_vacancies()
 
         pass
@@ -128,7 +200,7 @@ class HeadHunterAPI (Engine):
                 break
 
             # Необязательная задержка, но чтобы не нагружать сервисы hh, оставим. 5 сек мы может подождать
-                time.sleep(0.5)
+            time.sleep(0.5)
 
         # Создаем новый документ, записываем в него ответ запроса, после закрываем
         f = open(self.file, mode='w', encoding='utf8')
--- function/utils.py
+++ function/utils.py
@@ -1,39 +1,23 @@
 
-from data.API_connect import HeadHunterAPI, SuperJobAPI
+from data.API_connect import HeadHunterAPI, SuperJobAPI, Vacancy
 from requests import get, post, put, delete
 
-# response = get ('https://api.hh.ru/')
-# print(response)
+platforms=0
+while not (platforms in  [1, 2, 3]):
+    platforms = int(input('выберите платформу 1 - "HeadHunter", 2- "SuperJob", 3 - "HeadHunter"+"SuperJob"\n'))
+    if platforms in [1, 2, 3]:
+        print("платформа успешно введрена")
+    else:
+        print("ошибка ввода")
 
-# Создание экземпляра класса для работы с API сайтов с вакансиями
-# hh_api = HeadHunterAPI()
-# superjob_api = SuperJobAPI()
-
-# # Получение вакансий с разных платформ
-# hh_vacancies = HeadHunterAPI("Python")
-superjob_vacancies = SuperJobAPI("Python")
-print (superjob_vacancies.job_list)
+search_query = input('Введите поисковый запрос: ')
+top_n = int(input("Введите количество вакансий для вывода в топ N: "))
 
 
-# # Создание экземпляра класса для работы с API сайтов с вакансиями
-# hh_api = HeadHunterAPI()
-# superjob_api = SuperJobAPI()
-#
-# # Получение вакансий с разных платформ
-# hh_vacancies = hh_api.get_vacancies("Python")
-# superjob_vacancies = superjob_api.get_vacancies("Python")
-#
 # # Создание экземпляра класса для работы с вакансиями
-# vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
-#
-# # Сохранение информации о вакансиях в файл
-# json_saver = JSONSaver()
-# json_saver.add_vacancy(vacancy)
-# json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
-# json_saver.delete_vacancy(vacancy)
-#
-#
-#
-#
-# if __name__ == "__main__":
-#     user_interaction()
+vacancy = Vacancy(platforms,search_query,top_n)
+
+#Печать полученного списка
+for i in vacancy.list:
+    print(i)
+
